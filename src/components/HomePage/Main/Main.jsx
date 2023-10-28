a35db8ab895913cca20d3160422f9186e8ae30a9
SiLLeCToff <sillect1337@gmail.com>
2023-10-28 22:40:09 +0300

    img fixes again

--- src/components/HomePage/Main/Main.jsx
+++ src/components/HomePage/Main/Main.jsx
@@ -9,7 +9,7 @@ export const Main = () => {
         <h1 className="text-[#E8E8E8] text-[132px] font-bold max-sm:text-[85px]">JORDAN</h1>
         <h2 className="text-[#C10D06] text-[42px] font-extrabold">AIR</h2>
         </div>
-          <img src="../../../images/JordanMain.png" className="absolute 2xl:w-[1300px] h-full object-contain 2xl:-translate-y-[50px] md:-translate-y-[50px]" alt="Jordan"/>
+          <img src="/assets/JordanMain-3df281a7.png" className="absolute 2xl:w-[1300px] h-full object-contain 2xl:-translate-y-[50px] md:-translate-y-[50px]" alt="Jordan"/>
       </div>
       <div className="flex flex-col h-2/6 w-full justify-end items-end pr-[75px]">
           <h2 className="flex font-extrabold text-[#C10D06] text-[25px]">125 000 РУБ</h2>
