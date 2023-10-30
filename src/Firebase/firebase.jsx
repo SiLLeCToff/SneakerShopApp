d4decb626a967fb88f257e29a53d60d10c652b70
SiLLeCToff <sillect1337@gmail.com>
2023-10-31 01:22:56 +0300

    added firebase fixes x2

--- src/Firebase/firebase.jsx
+++ src/Firebase/firebase.jsx
@@ -1,19 +1,7 @@
-// import firebase from "firebase/app";
-// import "firebase/storage";
-//
-// const firebaseConfig = {
-//     apiKey: "YOUR_API_KEY",
-//     authDomain: "YOUR_AUTH_DOMAIN",
-//     projectId: "YOUR_PROJECT_ID",
-//     storageBucket: "YOUR_STORAGE_BUCKET",
-//     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//     appId: "YOUR_APP_ID",
-// };
-//
-// firebase.initializeApp(firebaseConfig);
-// const storage = firebase.storage();
-//
-// export default storage;
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
 const apiKey = import.meta.env.VITE_REACT_APP_FB_API_KEY;
 const Domain = import.meta.env.VITE_REACT_APP_FB_DOMAIN;
 const project = import.meta.env.VITE_REACT_APP_FB_PROJECT;
@@ -21,14 +9,7 @@ const bucket = import.meta.env.VITE_REACT_APP_FB_BUCKET;
 const sender = import.meta.env.VITE_REACT_APP_FB_SENDER;
 const idApp = import.meta.env.VITE_REACT_APP_FB_ID_APP;
 const measurement = import.meta.env.VITE_REACT_APP_FB_MEASUREMENY;
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: `${apiKey}`,
     authDomain: `${Domain}`,
@@ -39,8 +20,8 @@ const firebaseConfig = {
     measurementId: `${measurement}`
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
 
-export default firebase;
+export { storage, app, analytics };
