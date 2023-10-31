bd8c3d02722f12e3d3a836d62b922a3b3400ec42
SiLLeCToff <sillect1337@gmail.com>
2023-11-01 00:22:52 +0300

    fixes orderPage

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -20,7 +20,7 @@ const OrderPage = () => {
                 <div className="flex bg-[#FAFAFA] w-1/2 items-center justify-start gap-4 flex-col pt-[30px]">
                     {itemsFromBasket.map((item, index) => <div className="flex w-[80%] justify-start items-center gap-4" key={item.id}>
                         <div className="flex justify-center items-center">
-                        <img src={`${apiUrl}${encodeURIComponent(item.id)}?alt=media`} alt='photo' className="flex w-[150px] h-[120px] rounded-3xl object-contain border border-black border-opacity-5 "/>
+                        <img src={`${imgUrl}${encodeURIComponent(item.id)}?alt=media`} alt='photo' className="flex w-[150px] h-[120px] rounded-3xl object-contain border border-black border-opacity-5 "/>
                         </div>
                         <div className="flex justify-center flex-col gap-4 w-full">
                             <p className="flex whitespace-nowrap">{(brands.find(brand => brand.id === item.brandId) ? brands.find(brand => brand.id === item.brandId).name : '').toUpperCase()} {item.name}</p>
